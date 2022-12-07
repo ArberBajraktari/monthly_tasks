b14c09f245a0bbea44b726556006af61cfe3ae7f
Arber BAJRAKTARI <arber.bajraktari@rbinternational.com>
2022-12-07 16:24:37 +0100

    Executing python script

---
+++ src/Data_RBUK/send_data_to_RBUK.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from pandas import DataFrame
+import sqlalchemy
+from sqlalchemy import create_engine
+import getpass
+import win32com.client as win32
+
